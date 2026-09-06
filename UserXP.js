ddb64cf7e159e472b139954fe38f05edc426b7ee
PEPotes64 <andriramirez.terceroa@gmail.com>
2026-09-05 19:12:55 -0600

    Ey, lo dudo

--- UserXP.js
+++ UserXP.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 
 const userXpSchema = new mongoose.Schema({
-  userId: { type: String, required: true, unique: true },
+  userId: { type: String, required: true },
   guildId: { type: String, required: true },
   xp: { type: Number, default: 0 },
-  level: { type: Number, default: 1 }
+  level: { type: Number, default: 1 },
+  // 🕒 Campos para la expiración de roles comprados (1 día)
+  rolX2Hasta: { type: Date, default: null },
+  rolOcultoHasta: { type: Date, default: null }
 });
 
 module.exports = mongoose.model('UserXP', userXpSchema);
+
