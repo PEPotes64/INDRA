b16a0a8e29828dd7bc92b417b1c6c887311e655d
PEPotes64 <andriramirez.terceroa@gmail.com>
2026-09-06 12:32:06 -0600

    Negros de mrd

--- UserXP.js
+++ UserXP.js
@@ -5,9 +5,13 @@ const userXpSchema = new mongoose.Schema({
   guildId: { type: String, required: true },
   xp: { type: Number, default: 0 },
   level: { type: Number, default: 1 },
-  // 🕒 Campos para la expiración de roles comprados (1 día)
+  // ⏱️ Campos para la expiración de roles comprados (1 día)
   rolX2Hasta: { type: Date, default: null },
-  rolOcultoHasta: { type: Date, default: null }
+  rolOcultoHasta: { type: Date, default: null },
+  // ⏳ Campos para el cooldown de compra (2 días)
+  ultimoUsoX2: { type: Date, default: null },
+  ultimoUsoOculto: { type: Date, default: null },
+  ultimoUsoCaja: { type: Date, default: null }
 });
 
 module.exports = mongoose.model('UserXP', userXpSchema);
